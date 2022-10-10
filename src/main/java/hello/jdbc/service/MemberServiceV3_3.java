5ec6af63f74092d0f27df14a55358d7a85ace3f8
propark0709 <propark0709@gmail.com>
2022-10-10 19:44:08 +0900

    트랜잭션 스프링부트의 자동 리소스 등록

--- src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,37 +4,26 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import java.sql.Connection;
 import java.sql.SQLException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 /**
- * 트랜잭션 - 트랜잭션 템플릿
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
-public class MemberServiceV3_2 {
-//    private final DataSource dataSource;
-//    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
+public class MemberServiceV3_3 {
+
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager,
-                             MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        txTemplate.executeWithoutResult((status) -> {
-            try {
-                bizLogic(fromId,toId,money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId,toId,money);
     }
 
     private void bizLogic(String fromId, String toId, int money)
